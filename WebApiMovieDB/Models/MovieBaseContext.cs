87184a7e8d8f9de82ed4da5ec46f0d1302e0c2b8
Pavlo Osokin <pavlo.osokin@perfectial.com>
2021-04-28 13:54:12 +0300

    small updates

--- WebApiMovieDB/Models/MovieBaseContext.cs
+++ WebApiMovieDB/Models/MovieBaseContext.cs
@@ -66,9 +66,17 @@ namespace WebApiMovieDB.Models
 
             modelBuilder.Entity<UserInfo>(entity =>
             {
-                entity.HasKey(e => e.UserId);
+                entity.HasKey(e => e.UserId)
+                    .HasName("PK__UserInfo__1788CC4C1F5C1650");
 
-                entity.Property(e => e.UserId);
+                entity.Property(e => e.CreatedDate)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
 
                 entity.Property(e => e.FirstName)
                     .IsRequired()
@@ -80,19 +88,14 @@ namespace WebApiMovieDB.Models
                     .HasMaxLength(30)
                     .IsUnicode(false);
 
-                entity.Property(e => e.UserName)
-                    .IsRequired()
-                    .HasMaxLength(30)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.Email)
-                    .IsRequired()
-                    .HasMaxLength(50)
-                    .IsUnicode(false);
-
                 entity.Property(e => e.Password)
                     .IsRequired()
                     .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UserName)
+                    .IsRequired()
+                    .HasMaxLength(30)
                     .IsUnicode(false);
             });
 
